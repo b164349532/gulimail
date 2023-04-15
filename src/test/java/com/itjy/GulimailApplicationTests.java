e9cf31e30b4a85b7f231c460c37756a55d0553cd
cast <164349532@qq.com>
2023-04-15 21:00:54 +0800

    create project

---
+++ src/test/java/com/itjy/GulimailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itjy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
